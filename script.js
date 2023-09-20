41d43a860b0d585bee85beef7eaf4ef12524ca23
isurudotxyz <agisuru@icloud.com>
2023-09-20 03:50:35 +0200

    storing notes in local storage

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
 function updateStorage() {
   localStorage.setItem("notes", notesContainer.innerHTML);
 }
