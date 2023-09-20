cf5dcf33dd7434907e6bcd131343598e83bdcfa6
isurudotxyz <agisuru@icloud.com>
2023-09-20 03:31:40 +0200

    update storage when typing

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
 
 createBtn.addEventListener("click", () => {
-  console.log("i");
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
   inputBox.className = "input-box";
@@ -15,5 +17,13 @@ createBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === "p") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
   }
 });
